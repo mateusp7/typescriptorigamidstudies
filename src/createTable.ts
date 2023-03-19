9febeb62a8e4565c6d014df7935b7434901347ff
Mateus Raizen <mateusdev1337@gmail.com>
2023-03-19 19:31:31 -0300

    removing comments for function

--- src/createTable.ts
+++ src/createTable.ts
@@ -14,36 +14,4 @@ export default function createTable(transacoes: Transacao[]): void {
       </tr>
     `
   })
-
-  // const tbody = document.createElement("tbody")
-  // table.appendChild(tbody)
-
-  // const tr = document.createElement("tr")
-  // tr.classList.add("contentTables")
-
-  // tbody.appendChild(tr)
-
-  // const tdName = document.createElement("td")
-  // tdName.textContent = dados.nome
-
-  // const tdEmail = document.createElement("td")
-  // tdEmail.textContent = dados.email
-
-  // const tdCompra = document.createElement("td")
-  // tdCompra.textContent = dados.moeda
-
-  // const tdPagamento = document.createElement("td")
-  // tdPagamento.textContent = dados.pagamento
-
-  // const tdStatus = document.createElement("td")
-  // tdStatus.textContent = dados.status
-
-  // tr.appendChild(tdName)
-  // tr.appendChild(tdEmail)
-  // tr.appendChild(tdCompra)
-  // tr.appendChild(tdPagamento)
-  // tr.appendChild(tdStatus)
-
-  // table.appendChild(tr)
-  // return tr
 }
